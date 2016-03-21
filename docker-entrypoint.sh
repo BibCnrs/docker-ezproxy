fddfec1e68f63184833dccd45ca8ff5a8e042adc
ThieryMichel <thiery@marmelab.com>
2016-03-21 14:35:34 +0100

    use ezproxy -k to set the key and replace DNSMASQ_PORT_53_TCP_ADDR by more generic DNS_ADDR

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sed "1s/^/nameserver $DNSMASQ_PORT_53_TCP_ADDR\n/" /etc/resolv.conf > /tmp/resolv.conf
+sed "1s/^/nameserver $DNS_ADDR\n/" /etc/resolv.conf > /tmp/resolv.conf
 cp -f /tmp/resolv.conf /etc/
 /usr/local/ezproxy/ezproxy -m
+./ezproxy -k $EZ_PROXY_KEY
 cp -rf config/* .
 for f in *.php; do php $f > "${f%.*}.txt"; done
 /usr/local/ezproxy/ezproxy
